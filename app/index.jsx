e58e2f18421c9cdfe85316c0d4d64c6b6b4b115c
universeBuddy <rai358788@gmail.com>
2024-08-28 21:31:45 +0530

    layout added

--- app/index.jsx
+++ app/index.jsx
@@ -4,12 +4,12 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Ool </Text>
+    <View className="flex-1 items-center justify-center bg-whit e">
+      <Text className="text-3xl font-pblack">Ool </Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to{" "}
-      </Link>
+      </Link> 
     </View>
   );
 }
