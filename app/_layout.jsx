9cc6b6d7793c0b6640690a5b5817d4ae08f52ffd
universeBuddy <rai358788@gmail.com>
2024-08-28 19:52:35 +0530

    tailwind added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Stack } from "expo-router";
+import React from "react";
 
 const RootLayout = () => {
-  return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
+  return(
+    
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+
+    </Stack>
+  
   )
-}
+};
 
-export default RootLayout
-
-const styles = StyleSheet.create({
-  container :{
-    display:'flex',
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  }
-})
+export default RootLayout;
