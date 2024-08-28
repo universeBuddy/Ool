322ecf75f9c5a3c12c0f23bc2f6e23db6beb80d0
universeBuddy <rai358788@gmail.com>
2024-08-28 18:04:25 +0530

    adding expo and layout

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your </Text>
+      <Text>Open up App.js to start working on yo </Text>
       <StatusBar style="auto" />
     </View>
   );
