dd79e3ee5ae05af3a41ec885688aa36e295d2262
universeBuddy <rai358788@gmail.com>
2024-08-29 17:36:09 +0530

    Login complete

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,12 +3,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import FormField from "../../components/FormField";
 import { useState } from "react";
 import { images } from "../../constants";
-
+import CustomButtom from "../../components/CustomButton";
+import { Link } from "expo-router";
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submit = () => {};
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -18,23 +22,34 @@ const SignIn = () => {
             resizeMode="contain"
             className="w-[115px] h-[35px]"
           />
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Ool</Text>
-       
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Ool
+          </Text>
 
-        <FormField
-          title="Email"
-          value={form.email}
-          handleChangeText={(e) => setForm({ ...form, email: e })}
-          otherStyles="mt-7"
-          keyboardType="email-address"
-        />
-        <FormField
-          title="Password"
-          value={form.password}
-          handleChangeText={(e) => setForm({ ...form, password: e })}
-          otherStyles="mt-7"
-        />
-         </View>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+          <CustomButtom
+            title="Sign in"
+            handdlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">Don't have a account </Text>
+            <Link href="/sign-up" className="text-lg font-psemibold text-secondary">SignUp</Link>
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
