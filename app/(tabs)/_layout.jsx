2e641c20db67071b0119a25dc8758c1558da8f5e
universeBuddy <rai358788@gmail.com>
2024-08-29 07:33:17 +0530

    adding more tabs

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import { icons } from "../../constants";
+import { Tabs } from "expo-router";
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View>
-      <Image source={icon} resizeMode="contain"
-      tintColor={color}
-      className="w-6 h-6"
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
       />
-      <Text className={`${focused ?  'font-psemibold':'font-pregular'} text-xs`}>
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
         {name}
       </Text>
     </View>
@@ -18,7 +23,52 @@ const TabIcon = ({ icon, color, name, focused }) => {
 const TabsLayout = () => {
   return (
     <>
-      <Tabs>
+      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
         <Tabs.Screen
           name="home"
           options={{
--- app/index.jsx
+++ app/index.jsx
@@ -4,11 +4,11 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-whit e">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Ool </Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
-        Go to{" "}
+        Go to Home{" "}
       </Link>
     </View>
   );
