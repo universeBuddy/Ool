847863016a17857c5f77457426dc25e710ba2eb4
universeBuddy <rai358788@gmail.com>
2024-08-31 12:40:12 +0530

    video player added

--- components/Tranding.jsx
+++ components/Tranding.jsx
@@ -1,14 +1,21 @@
-import { View, Text, FlatList, TouchableOpacity, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
 import React, { useState } from "react";
 import * as Animatable from "react-native-animatable";
-;
-
+import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
 const zoomIn = {
   0: {
     scale: 0.9,
   },
   1: {
-    scale: 1,
+    scale: 1.0,
   },
 };
 
@@ -23,6 +30,7 @@ const zoomOut = {
 
 const TrandingItem = ({ activeItem, item }) => {
   const [play, setPlay] = useState(false);
+  console.log(activeItem);
   return (
     <Animatable.View
       className="mr-5"
@@ -30,15 +38,35 @@ const TrandingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
           activeOpacity={0.7}
           onPress={() => setPlay(true)}
         >
-          <ImageBackground source={{ uri:item.thumbnail }}
-          className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40" resizeMode="cover" />
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
+          />
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
         </TouchableOpacity>
       )}
     </Animatable.View>
@@ -47,6 +75,11 @@ const TrandingItem = ({ activeItem, item }) => {
 const Tranding = ({ posts }) => {
   const [activeItem, setActiveItem] = useState(posts[0]);
 
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
   return (
     <FlatList
       data={posts}
@@ -54,6 +87,11 @@ const Tranding = ({ posts }) => {
       renderItem={({ item }) => (
         <TrandingItem activeItem={activeItem} item={item} />
       )}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      onViewableItemsChanged={viewableItemsChanged}
+      contentOffset={{ x: 170 }}
       horizontal
     />
   );
