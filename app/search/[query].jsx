a7c1ecfca99862960aa28d3b7132bcad0c3fa57a
universeBuddy <rai358788@gmail.com>
2024-08-31 16:14:50 +0530

    search result added

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,17 +1,50 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyState from "../../components/EmptyState";
+import SerachInput from "../../components/SearchInput";
+import VideoCard from "../../components/VideoCard";
+import { getAllPosts, searchPosts } from "../../lib/appwrite";
+import useAppWrite from "../../lib/useAppwrite";
 
 const Search = () => {
-const {query} = useLocalSearchParams();
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppWrite(() => searchPosts(query));
 
+  useEffect(() => { 
+    refetch();
+  }, [query]);
+
+  console.log(posts);
   return (
-<SafeAreaView className="bg-primary h-full" >
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-6 space-y-6">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Result
+            </Text>
 
-  <Text className = "text-3xl text-white">{query}</Text>
+            <Text className="text-2xl font-psemibold text-white">{query}</Text>
 
-</SafeAreaView>
-  )
-}
+            <View className="mt-6 mb-8">
+              <SerachInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Video Found"
+            subtitle="No Video found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Search
+export default Search;
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
-import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, Image, TextInput, TouchableOpacity, View } from "react-native";
 import { icons } from "../constants";
-import {usePathname} from 'expo-router'
-const SerachInput = () => {
+import { router, usePathname } from "expo-router";
+
+
+
+
+const SerachInput = ({initialQuery}) => {
 
   const pathname = usePathname();
-const [query,setQuery] = useState()
+  const [query, setQuery] = useState(initialQuery || '');
+
+
   return (
     <View className="border-2 border-black-200 w-full h-14 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
       <TextInput
@@ -13,18 +19,21 @@ const [query,setQuery] = useState()
         value={query}
         placeholder="Serach for a video topic"
         placeholderTextColor="#CDCDE0"
-        onChangeText={(e) =>setQuery(e)}
-       
+        onChangeText={(e) => setQuery(e)}
       />
 
       <TouchableOpacity
-      onPress={()=>{
-        if(!query){
-          return Alert.alert('Missing Query', "Please input something to search results across database")
-        }
-        if(pathname.startsWith('/search')) router.setParams({query})
-          else router.push (`/search/${query}`)
-      }}>
+        onPress={() => {
+          if (!query) {
+            return Alert.alert(
+              "Missing Query",
+              "Please input something to search results across database"
+            );
+          }
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
+      >
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
@@ -32,5 +41,3 @@ const [query,setQuery] = useState()
 };
 
 export default SerachInput;
-
-
