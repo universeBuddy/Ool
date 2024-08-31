7fee89a7b290052f80c7620befd9f31408d0f5b9
universeBuddy <rai358788@gmail.com>
2024-08-31 18:54:44 +0530

    currnt user added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -15,7 +15,7 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppWrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
-
+import { useGlobalContext } from "../../context/GlobalProvider";
 const Home = () => {
  
 const {data:posts,refetch} = useAppWrite(getAllPosts);
@@ -45,10 +45,10 @@ const {data:latestPosts} = useAppWrite(getLatestPosts);
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
+                  Welcome back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Mosfet
+              {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,20 +1,28 @@
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { FlatList, Image, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import EmptyState from "../../components/EmptyState";
 import VideoCard from "../../components/VideoCard";
 import { useGlobalContext } from "../../context/GlobalProvider";
-import { getUserPosts } from "../../lib/appwrite";
+import { getUserPosts, signOut } from "../../lib/appwrite";
 import useAppWrite from "../../lib/useAppwrite";
 import { icons } from "../../constants";
+import InfoBox from "../../components/InfoBox";
 
 const Profile = () => {
   const { query } = useLocalSearchParams();
   const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: posts } = useAppWrite(() => getUserPosts(user.$id));
 
-  const logout = () => {};
+  const logout = async () => {
+
+    await signOut();
+    setUser(null);
+    setIsLoggedIn(false);
+    router.replace('/sign-in')
+
+  };
   return (
     <SafeAreaView className="bg-primary h-full">
       <FlatList
@@ -33,12 +41,33 @@ const Profile = () => {
                 className="w-6 h-6"
               />
             </TouchableOpacity>
-            <View className="w-16 h-16 border border-secondary-100 rounded-lg justify-center items-center">
+            <View className="w-16 h-16 border border-secondary rounded-lg justify-center items-center">
               <Image
                 source={{ uri: user?.avatar }}
                 className="w-[90%]  h-[90%] rounded-lg"
                 resizeMode="cover"
               />
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles="mt-5"
+              titleStyles="text-lg"
+            />
+
+            <View className="mt-4 flex-row">
+            <InfoBox
+              title={posts.length || 0 }
+              subtitle="Posts"
+              containerStyles="mr-10"
+              titleStyles="text-xl"
+            />
+            <InfoBox
+              title= "1.2k"
+              subtitle="Followers"
+              titleStyles="text-xl"
+            />
+            
             </View>
           </View>
         )}
